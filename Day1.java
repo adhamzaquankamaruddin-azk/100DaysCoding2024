15eb9affa5048c5dd4f17f19b9faf95687bcb2dc
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-09-02 11:11:31 +0800

    Create Day1.java
    Biodata

---
+++ Day1.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println ("Nama = Adham Zaquan Kamaruddin");
+		System.out.println ("Alamat = Laliko");
+		System.out.println ("Asal Sekolah = UPTD SMK Negeri Labuang");
+	}
+}
