1d02513543b94fd8d70d735edf96bafcae7d67ef
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-09-04 15:19:33 +0800

    Update Day1.java
    Class

--- Day1.java
+++ Day1.java
@@ -1,7 +1,3 @@
-public class Main {
-	public static void main(String[] args) {
-		System.out.println ("Nama = Adham Zaquan Kamaruddin");
-		System.out.println ("Alamat = Laliko");
-		System.out.println ("Asal Sekolah = UPTD SMK Negeri Labuang");
-	}
+public class Day1 {
+	
 }
