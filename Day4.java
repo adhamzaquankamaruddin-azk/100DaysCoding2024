86f3a858dfe41c485d1c5f3d85f6aaa4d6869524
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-09-05 12:17:50 +0800

    Create Day4.java
    System.out.print dan System.out.printf

---
+++ Day4.java
@@ -0,0 +1,10 @@
+public class Day4 {
+	public static void main(String[] args) {
+		System.out.print("Nama ku Adham \n");
+		System.out.print("Aku tinggal di Laliko \n");
+		System.out.print("Aku berumur 19 tahun \n");
+		System.out.printf("Aku anak %s %n","tengah");
+		System.out.printf("Aku %d bersaudara %n", 3);
+		System.out.printf("Golongan darah ku %c %n", 'O');
+	}
+}
