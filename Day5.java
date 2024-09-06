e6d5cf6805df98b4626da8d74755e30e93b34039
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-09-06 08:51:10 +0800

    Create Day5.java
    Komentar

---
+++ Day5.java
@@ -0,0 +1,10 @@
+public class Day5 {
+    
+    public static void main(String[] args) {
+        //Ini adalah contoh penggunaan komentar satu baris
+        /* Ini adalah contoh penggunaan komentar
+         untuk lebih dari satu baris
+         */
+    }
+    
+}
