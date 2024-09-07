c9a314476b616e136cf54c3f65150e53acf688e3
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-09-07 08:15:46 +0800

    Create Day6.java
    Deklarasi Variabel

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class Day6 {
+    
+    public static void main(String[] args) {
+        double tinggi_badan;
+        float berat_badan;
+        char golongan_darah;
+        boolean hidup;
+    }
+    
+}
