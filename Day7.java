da62fc7923a5274e453b79b6d8f625c47b0ff166
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-09-08 09:50:46 +0800

    Create Day7.java
    Inisialisasi variabel

---
+++ Day7.java
@@ -0,0 +1,15 @@
+public class Day7 {
+    
+    public static void main(String[] args) {
+        double tinggi_badan = 153.6;
+        float berat_badan = 46.2f;
+        char golongan_darah = 'O';
+        boolean hidup = true;
+        
+        System.out.println("Tinggi Badan : " + tinggi_badan);
+        System.out.println("Berat Badan : " + berat_badan);
+        System.out.println("Golongan Darah : " + golongan_darah);
+        System.out.println("Masih hidup : " + hidup);
+    }
+    
+}
