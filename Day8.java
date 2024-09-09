5b3dca070a8cbeb52ad33281d8f73e24e24a3c93
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-09-09 11:41:42 +0800

    Create Day8.java
    Update variabel

---
+++ Day8.java
@@ -0,0 +1,30 @@
+public class Day8 {
+    
+    public static void main(String[] args) {
+        String nama = "Arwah";
+        System.out.println("Sebelum update : " + nama);
+        nama = "Adham";
+        System.out.println("Setelah update : " + nama);
+        
+        int umur = 2;
+        System.out.println("Sebelum update : " + umur);
+        umur = 19;
+        System.out.println("Setelah update : " + umur);
+        
+        char jenis_kelamin = '?';
+        System.out.println("Sebelum update : " + jenis_kelamin);
+        jenis_kelamin = 'L';
+        System.out.println("Setelah update : " + jenis_kelamin);
+        
+        double tinggi_badan = 126.3;
+        System.out.println("Sebelum update : " + tinggi_badan);
+        tinggi_badan = 153.6;
+        System.out.println("Setelah update : " + tinggi_badan);
+        
+        float berat_badan = 39.6f;
+        System.out.println("Sebelum update : " + berat_badan);
+        berat_badan = 46.2f;
+        System.out.println("Setelah update : " + berat_badan);
+    }
+    
+}
