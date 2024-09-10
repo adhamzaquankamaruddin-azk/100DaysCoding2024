c6e3937b9930bcb8677ea0fded11b70ec9a9e66c
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-09-10 17:16:13 +0800

    Create Day9.java
    Variabel Final

---
+++ Day9.java
@@ -0,0 +1,17 @@
+public class Day9 {
+    
+    public static void main(String[] args) {
+        final String nama = "Adham";
+        final int umur = 19;
+        final char golongan_darah = 'O';
+        final double tinggi_badan = 153.6;
+        final float berat_badan = 46.2f;
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+        System.out.println("Golongan Darah : " + golongan_darah);
+        System.out.println("Tinggi Badan : " + tinggi_badan);
+        System.out.println("Berat Badan : " + berat_badan);
+    }
+    
+}
