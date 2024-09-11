49fedf2db97be67f5f75793d60954253e5b6f8e7
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-09-12 03:06:11 +0800

    Create Day11.java
    Tipe Data Short

---
+++ Day11.java
@@ -0,0 +1,8 @@
+public class Day11 {
+    
+    public static void main(String[] args) {
+        short fav_number = 1326;
+        System.out.println("Nomor Favorit : " + fav_number);
+    }
+    
+}
