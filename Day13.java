0400e71cfc18af183fa4b8afd4066383507e7083
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-09-14 10:07:03 +0800

    Create Day13.java
    Evaluasi Day1 - Day12

---
+++ Day13.java
@@ -0,0 +1,25 @@
+public class Day13 {
+	public static void main(String[] args) {
+		String nama = "Aryan";
+    short umurku = 19;
+    int umurnenek = 100;
+    byte umurkakek = 120;
+        System.out.println("Biodata Mahasiswa 1");
+        System.out.printf("Nama \t\t : " + nama + "%n");
+        System.out.printf("Umur \t\t : " + umurku + " Tahun %n");
+        System.out.printf("UmurNenek \t : " + umurnenek + " Tahun %n");
+        System.out.printf("UmurKakek \t : " + umurkakek + " Tahun %n");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    
+    nama = "Adham";
+    umurku = 20;
+    umurnenek = 101;
+    umurkakek = 118;
+        System.out.println("Biodata Mahasiswa 2");
+        System.out.printf("Nama \t\t : " + nama + "%n");
+        System.out.printf("Umur \t\t : " + umurku + " Tahun %n");
+        System.out.printf("UmurNenek \t : " + umurnenek + " Tahun %n");
+        System.out.printf("UmurKakek \t : " + umurkakek + " Tahun %n");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	}
+}
