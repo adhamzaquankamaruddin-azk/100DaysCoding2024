064ba0a8fe19c9eb963af481d7040c7cb7940bd8
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-09-15 20:52:38 +0800

    Create Day14.java
    Tipe data Long

---
+++ Day14.java
@@ -0,0 +1,8 @@
+public class Day14{
+    public static void main(String[] args) {
+        //Batas memori untuk tipe data long:
+        //2.147.483.647
+        long id_ml = 224695405;
+        System.out.println(id_ml);
+    }
+}
