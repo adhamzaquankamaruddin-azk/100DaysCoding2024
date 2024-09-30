959f4e8a0f3e90b4a8457ef91956bf0fb99b3375
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-09-30 21:26:41 +0800

    Create Day29.java
    Tipe data reference: BigInteger

---
+++ Day29.java
@@ -0,0 +1,9 @@
+import java.math.BigInteger;
+public class Day29 {
+    
+    public static void main(String[] args) {
+        BigInteger angka = new BigInteger("13012005");
+        System.out.println(angka);
+    }
+    
+}
