5ebb63cfa86e8d00386d25559e1639876e0af2da
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-02 07:28:44 +0800

    Create Day31.java
    Konversi String ke tipe data primitif

---
+++ Day31.java
@@ -0,0 +1,9 @@
+public class Day31 {
+    
+    public static void main(String[] args) {
+        String angka = "13";
+        int angkalagi = Integer.valueOf(angka);
+        System.out.println(angkalagi);
+    }
+    
+}
