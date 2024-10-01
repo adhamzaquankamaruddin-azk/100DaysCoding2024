f5a6b33f04bb56fbd21504945b18197fb7447836
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-01 22:28:48 +0800

    Create Day30.java
    Tipe data reference: BigDecimal

---
+++ Day30.java
@@ -0,0 +1,9 @@
+import java.math.BigDecimal;
+public class Day30 {
+    
+    public static void main(String[] args) {
+        BigDecimal angka = new BigDecimal("1.326.000.000");
+        System.out.println(angka);
+    }
+    
+}
