e269edd099796485a957e7482224593321c5e148
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-03 08:35:26 +0800

    Create Day32.java
    Konversi tipe data primitif ke String

---
+++ Day32.java
@@ -0,0 +1,9 @@
+public class Day32 {
+    
+    public static void main(String[] args) {
+        int angka = 1326;
+        String angkalagi = angka + "";
+        System.out.println(angkalagi);
+    }
+    
+}
