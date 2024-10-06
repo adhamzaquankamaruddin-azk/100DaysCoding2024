93d8ea87f8d0659cd7fcb9039649aa2edcaf0ad4
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-06 21:06:55 +0800

    Create Day35.java
    Operator aritmatika: Penjumlahan & Pengurangan

---
+++ Day35.java
@@ -0,0 +1,15 @@
+public class Day35 {
+    
+    public static void main(String[] args) {
+        //Penjumlahan
+        int a = 6;
+        int z = 7;
+        System.out.println(a + z);
+        
+        //Pengurangan
+        int n = 32;
+        int h = 6;
+        System.out.println(n - h);
+    }
+    
+}
