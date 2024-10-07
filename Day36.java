f6ba2bdd2e30229ee08261a69cec5e4950454946
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-07 21:12:29 +0800

    Create Day36.java
    Operator Aritmatika: Perkalian, Pembagian dan Modulus / Sisa Bagi

---
+++ Day36.java
@@ -0,0 +1,20 @@
+public class Day36 {
+    
+    public static void main(String[] args) {
+        //Perkalian
+        int a = 13;
+        int z = 2;
+        System.out.println(a * z);
+        
+        //Pembagian
+        int n = 26;
+        int h = 2;
+        System.out.println(n / h);
+        
+        //Madulus / Sisa Bagi
+        int kl = 10;
+        int kp = 2;
+        System.out.println(kl % kp);
+    }
+    
+}
