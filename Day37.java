5f9b4f56c2a59df5942a072c5b3fe32da6dff492
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-08 20:37:11 +0800

    Create Day37.java
    Operator Penugasan : Pengisian Nilai, Pengisian dan 
    Penambahan, Pengisian dan Pengurangan

---
+++ Day37.java
@@ -0,0 +1,16 @@
+public class Day37 {
+    public static void main(String[] args) {
+        
+        int jumlahBakso = 13;
+        System.out.println("Jumlah Bakso = " + jumlahBakso);
+        
+        jumlahBakso += 2;
+        System.out.println("Penambahan: ");
+        System.out.println("Jumlah Bakso = " + jumlahBakso);
+        
+        jumlahBakso -= 6;
+        System.out.println("Pengurangan: ");
+        System.out.println("Jumlah Bakso = " + jumlahBakso);
+   
+    }
+}
