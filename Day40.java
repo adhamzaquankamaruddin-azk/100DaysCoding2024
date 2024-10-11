fecbabddb7c1649d2b4a796ec2ec46a59b851370
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-11 20:17:11 +0800

    Create Day40.java
    Operator Perbandingan: Lebih besar dan lebih kecil

---
+++ Day40.java
@@ -0,0 +1,10 @@
+public class Day40 {
+    
+    public static void main(String[] args) {
+        int a = 13;
+        int n = 26;
+        System.out.println(a > n);
+        System.out.println(a < n);
+    }
+    
+}
