ab375ef179cbbe5c33bc81e3afacf2615e2d91c8
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-13 21:01:59 +0800

    Create Day42.java
    Operator Perbandingan: Sama dengan dan tidak sama dengan

---
+++ Day42.java
@@ -0,0 +1,10 @@
+public class Day42 {
+    
+    public static void main(String[] args) {
+        int t = 13;
+        int f = 26;
+        System.out.println(t == f);
+        System.out.println(t != f);
+    }
+    
+}
