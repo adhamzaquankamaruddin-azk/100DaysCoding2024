5cface490834c475824df5fee5bbad4ba930e0ac
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-14 22:37:45 +0800

    Create Day43.java
    Operator Perbandingan : Lebih besar sama dengan dan lebih 
    kecil sama dengan:

---
+++ Day43.java
@@ -0,0 +1,10 @@
+public class Day43 {
+    
+    public static void main(String[] args) {
+        int te = 13;
+        int ef = 26;
+        System.out.println(te >= ef);
+        System.out.println(te <= ef);
+    }
+    
+}
