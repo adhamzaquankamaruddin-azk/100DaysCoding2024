dc425a29046e28877501764ca526a3e8d8243ac8
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-15 23:14:53 +0800

    Create Day44.java
    Operator Logika AND/&&

---
+++ Day44.java
@@ -0,0 +1,11 @@
+public class Day44 {
+    
+    public static void main(String[] args) {
+        int te = 13;
+        int ef = 26;
+        boolean rg = te < ef && ef != te;
+        
+        System.out.println(rg);
+    }
+    
+}
