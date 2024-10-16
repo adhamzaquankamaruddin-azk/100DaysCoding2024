7863cb5013ed02261c3026bedecdfab04dda6c53
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-16 20:53:50 +0800

    Create Day45.java
    Operator Logika: OR / ||

---
+++ Day45.java
@@ -0,0 +1,11 @@
+public class Day45 {
+    
+    public static void main(String[] args) {
+        boolean or_true = 13 == 26 || 13 < 26;
+        boolean or_false = 13 >= 26 || 13 == 26;
+        
+        System.out.println(or_true);
+        System.out.println(or_false);
+    }
+    
+}
