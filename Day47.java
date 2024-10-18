eaf9e3f41d169bf642ef4c439b6c10a47c031ba8
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-18 21:16:13 +0800

    Create Day47.java
    Operator Percabangan: if

---
+++ Day47.java
@@ -0,0 +1,10 @@
+public class Day47 {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        if (a > 5) {
+            System.out.println("a lebih besar dari 5");
+        }    
+    }
+    
+}
