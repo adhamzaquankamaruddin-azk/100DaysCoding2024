7a3d0f24da4250578211dcc034b65eac3f0abb63
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-19 22:50:40 +0800

    Create Day48.java
    Operator Logika: if, else

---
+++ Day48.java
@@ -0,0 +1,12 @@
+public class Day48 {
+    
+    public static void main(String[] args) {
+        int a = 2;
+        if (a > 5) {
+            System.out.println("a lebih besar dari 5");
+        } else {
+            System.out.println("a lebih kecil dari 5");
+        }
+    }
+    
+}
