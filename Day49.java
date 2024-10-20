1bbc58038692677a104c4c7e02632f05415385a1
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-20 22:26:52 +0800

    Create Day49.java
    Logika Percabangan: if - else if

---
+++ Day49.java
@@ -0,0 +1,12 @@
+public class Day49 {
+    
+    public static void main(String[] args) {
+        int umur = 13;
+        if (umur <= 12) {
+            System.out.println("anak-anak");
+        } else if (umur <= 17){
+            System.out.println("remaja");
+        } 
+    }
+    
+}
