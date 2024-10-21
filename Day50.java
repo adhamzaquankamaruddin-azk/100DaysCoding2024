270a3f9d463d66a751319056b6aea9af708cef20
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-21 22:52:35 +0800

    Create Day50.java
    Operator Percabangan: if - else if - else

---
+++ Day50.java
@@ -0,0 +1,15 @@
+public class Day50 {
+    
+    public static void main(String[] args) {
+        int umur = 26;
+        if (umur <= 12) {
+            System.out.println("anak-anak");
+        } else if (umur <= 17){
+            System.out.println("remaja");
+        } else {
+            System.out.println("dewasa");
+        }
+        
+    }
+    
+}
