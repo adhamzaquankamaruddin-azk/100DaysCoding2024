7645b9e9eb6634028031c99bff6d407832f321f8
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-29 18:26:51 +0800

    Create Day58.java
    Perulangan loop : while decrement

---
+++ Day58.java
@@ -0,0 +1,11 @@
+public class Day58 {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        while (a > 0) {
+            System.out.println(a);
+            a--;
+        }
+    }
+    
+}
