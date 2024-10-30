edede34f0b0a6ba04aee6d5aae177621abb91583
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-10-30 23:41:28 +0800

    Create Day59.java
    Perulangan//Looping: for each

---
+++ Day59.java
@@ -0,0 +1,10 @@
+public class Day59 {
+    public static void main(String[] args) {
+        
+        int[] n = {1, 2, 3, 4, 5};
+        
+        for (int i : n) {
+            System.out.println(i);
+        }
+    }
+}
