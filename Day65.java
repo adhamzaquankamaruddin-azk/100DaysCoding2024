a4fa87dbfd600a67ca96652f3cc2acc57407343e
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-05 21:31:35 +0800

    Create Day65.java
    Nested loop

---
+++ Day65.java
@@ -0,0 +1,12 @@
+public class Day65 {
+    
+    public static void main(String[] args) {
+        for(int i = 1; i <= 3; i++) {
+            System.out.println("Perkalian : " + i);
+            for(int a = 1; a <= 5; a++) {
+                System.out.println(a + " x " + i + " = " + (i * a));
+            }
+        }
+    }
+    
+}
