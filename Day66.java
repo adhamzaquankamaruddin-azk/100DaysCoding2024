6fd9fa66ac8797ef4b45afe2503085dcc88de7a3
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-06 21:59:31 +0800

    Create Day66.java
    Pola persegi dan persegi panjang

---
+++ Day66.java
@@ -0,0 +1,20 @@
+public class Day66 {
+    
+    public static void main(String[] args) {
+        System.out.println("#Pola Persegi");
+        for(int i = 1; i <= 5; i++) {
+            for(int a = 1; a <= 5; a++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        System.out.println("#Pola Persegi Panjang");
+        for(int z = 1; z <= 5; z++) {
+            for(int k = 1; k <= 10; k++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
