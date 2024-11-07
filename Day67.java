0c896426f592b41fecc5354f7f792d9e64f7a913
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-07 20:25:44 +0800

    Create Day67.java
    Method void

---
+++ Day67.java
@@ -0,0 +1,11 @@
+public class Day67 {
+    
+    public static void main(String[] args) {
+        hitungLuasPersegi(2);
+    }
+    public static void hitungLuasPersegi(int sisi) {
+        int luas = sisi * sisi;
+        System.out.println("Luas persegi: " + luas);
+    }
+
+}
