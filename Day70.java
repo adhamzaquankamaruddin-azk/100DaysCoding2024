0dccda0f631810fd02b05f785f521bf399fe9912
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-10 20:00:19 +0800

    Create Day70.java
    Method parameter

---
+++ Day70.java
@@ -0,0 +1,10 @@
+public class Day70 {
+    
+    public static String mahasiswa(String nama, int angkatan) {
+        return nama + " adalah mahasiswa angkatan " + angkatan;
+    }
+    public static void main(String[] args) {
+        System.out.println(mahasiswa("Adham", 24));
+    }
+    
+}
