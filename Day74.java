6b315d28cb0358fa4b68bc69f49f55ca571b05e8
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-14 20:14:50 +0800

    Create Day74.java
    String method: length

---
+++ Day74.java
@@ -0,0 +1,8 @@
+public class Day74 {
+    
+    public static void main(String[] args) {
+        String nama = "adham";
+        char huruf = nama.charAt(3);
+        System.out.println(huruf);
+    }
+}
