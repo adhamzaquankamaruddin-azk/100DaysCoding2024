6731d2f238bbf750ca723358b8a024f3f86c9dbf
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-15 00:17:45 +0800

    Create Day75.java
    String method: toLowerCase()

---
+++ Day75.java
@@ -0,0 +1,8 @@
+public class Day75 {
+    
+    public static void main(String[] args) {
+        String besar = "ADHAM Zaquan KaMarUddiN";
+        String kecil = besar.toLowerCase();
+        System.out.println(kecil);
+    }
+}
