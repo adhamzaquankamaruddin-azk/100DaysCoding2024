7fbc00c8b159398c477aefcbf30a2190e6c89173
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-16 18:28:03 +0800

    Create Day76.java
    String method: toUpperCase()

---
+++ Day76.java
@@ -0,0 +1,8 @@
+public class Day76 {
+    
+    public static void main(String[] args) {
+        String random = "adham zAQUAN kAmARuDDIn";
+        String besar = random.toUpperCase();
+        System.out.println(besar);
+    }
+}
