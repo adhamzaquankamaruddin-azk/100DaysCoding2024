fba26f2932ab455343aa2831e4cae6c7fb5ecef2
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-19 21:03:09 +0800

    Create Day79.java
    String method: toString()

---
+++ Day79.java
@@ -0,0 +1,9 @@
+public class Day79 {
+    
+    public static void main(String[] args) {
+        float dariInt = 130105.2f;
+        String jadiString = Float.toString(dariInt);
+        
+        System.out.println(jadiString);
+    }
+}
