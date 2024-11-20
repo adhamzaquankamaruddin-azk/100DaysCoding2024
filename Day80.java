f43ff9f1046e85218dfcdf3e8950d49a7339de86
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-20 19:30:49 +0800

    Create Day80.java
    String method: trim()

---
+++ Day80.java
@@ -0,0 +1,9 @@
+public class Day80 {
+    
+    public static void main(String[] args) {
+        String nama = "      Adham      ";
+        String namaTrim = nama.trim();
+        
+        System.out.println("Nama : " + namaTrim);
+    }
+}
