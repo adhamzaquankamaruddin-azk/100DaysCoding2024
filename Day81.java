56a41961ec4becc0527bfb55d44b06c7441ebc9c
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-21 20:43:27 +0800

    Create Day81.java
    String method: valueOf()

---
+++ Day81.java
@@ -0,0 +1,10 @@
+public class Day81 {
+    
+    public static void main(String[] args) {
+        String dariString = "13";
+        short jadiShort = Short.valueOf(dariString);
+        
+        System.out.println(jadiShort);
+    }
+    
+}
