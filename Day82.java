fe9ea1905cd20fbfb4ac756a89c001eeacd0b25d
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-22 21:35:33 +0800

    Create Day82.java
    Method ceil, floor dan round

---
+++ Day82.java
@@ -0,0 +1,11 @@
+public class Day82 {
+    public static void main(String[] args) {
+        double versiCeil = 1.1;
+        double versiFloor = 1.1;
+        double versiRound = 1.1;
+        
+        System.out.println("Versi Ceil = " + Math.ceil(versiCeil));
+        System.out.println("Versi Floor = " + Math.floor(versiFloor));
+        System.out.println("Versi Round = " + Math.round(versiRound));
+    }
+}
