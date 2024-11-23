66458efe206e542dab05a59d424c9a5162ccaddc
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-23 23:39:55 +0800

    Create Day83.java
    Array 1 dimensi

---
+++ Day83.java
@@ -0,0 +1,7 @@
+public class Day83 {
+    public static void main(String[] args) {
+        String[] hewan = {"naga", "kupu-kupu", "kucing", "serigala", "kura-kura"};
+        
+        System.out.println(hewan[2]);
+    }
+}
