a3cca9cbb015dfff759882d55f7c8514f3d80fe2
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-27 02:03:30 +0800

    Create Day87.java
    Mengubah Nilai Array satu dimensi pada index Tertentu

---
+++ Day87.java
@@ -0,0 +1,12 @@
+public class Day87 {
+    public static void main(String[] args) {
+        char[] kelasInformatika24 = {'A', 'B', 'C', 'D'};
+        System.out.print("Sebelum di ubah, nilai index 1 adalah: ");
+        System.out.println(kelasInformatika24[1]);
+        
+        kelasInformatika24[1] = 'Z';
+        
+        System.out.print("Setelah di ubah, nilai index 1 adalah: ");
+        System.out.println(kelasInformatika24[1]);
+    }
+}
