d7601e84c9bd66a38c0397a3684434acefeae644
adhamzaquankamaruddin-azk <adhamzaquankamaruddin@gmail.com>
2024-11-30 21:37:43 +0800

    Create Day90.java
    Soal

---
+++ Day90.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Day91 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        //Modal Awal
+        System.out.print("Masukkan Modal Awal: ");
+        double modalAwal = sc.nextDouble();
+        
+        //Lama investasi, berapa tahun investasi nya
+        System.out.print("Masukkan Lama Investasi: ");
+        int lama = sc.nextInt();
+        
+        //Keuntungan investasi di setiap tahun nya
+        //dengan modal yang telah di tentukan
+        double keuntungan = 0;
+        
+        //Ini untuk menghitung berapa keuntungan yang
+        //di dapat setiap tahun nya
+        for (int i = 1; i <= lama; i++) {
+            keuntungan = modalAwal * 0.05;
+            modalAwal += keuntungan;
+        }
+        System.out.println(modalAwal);
+    }
+}
